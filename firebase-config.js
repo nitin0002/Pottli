7291d3c04bee3448672f14e84e656dcb65e7c1eb
Nitin Tomar <87395304+nitin0002@users.noreply.github.com>
2025-03-20 16:03:11 +0530

    Add Firebase configuration and VSCode settings

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDL6j3l4BnXjNc0vCg-s23CNNmi8oaUQpM",
+    authDomain: "pottli-b9cc5.firebaseapp.com",
+    projectId: "pottli-b9cc5",
+    storageBucket: "pottli-b9cc5.firebasestorage.app",
+    messagingSenderId: "686260705601",
+    appId: "1:686260705601:web:12b8607e2a6717790329c0",
+    measurementId: "G-98WXCYS92Y"
+  };
--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
-  import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+import { firebaseConfig } from "./firebase-config.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDL6j3l4BnXjNc0vCg-s23CNNmi8oaUQpM",
-    authDomain: "pottli-b9cc5.firebaseapp.com",
-    projectId: "pottli-b9cc5",
-    storageBucket: "pottli-b9cc5.firebasestorage.app",
-    messagingSenderId: "686260705601",
-    appId: "1:686260705601:web:12b8607e2a6717790329c0",
-    measurementId: "G-98WXCYS92Y"
-  };
+
+  
 
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
